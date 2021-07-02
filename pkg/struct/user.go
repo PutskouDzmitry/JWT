efa3397aab168ae56e240fe3bb3a1e92e3b72b7b
PutskouDzmitry <70838579+PutskouDzmitry@users.noreply.github.com>
2021-07-02 11:16:35 +0300

    Delete user.go

--- pkg/struct/user.go
+++
@@ -1,7 +0,0 @@
-package _struct
-
-type User struct {
-	Id string `json:"id" bson:"_id"`
-	Username string `json:"username" bson:"username" binding:"required"`
-	Password string `json:"password" bson:"password" binding:"required"`
-}
